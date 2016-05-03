fcc1196eca9fdfa38986e9a2e6dd064c5d8eb328
Jim Barritt <jim@planet-ix.com>
2016-05-03 19:44:31 +0100

    Got basic parsing

--- src/status_codes/core.clj
+++ src/status_codes/core.clj
@@ -12,7 +12,37 @@
 
 (def body (:content (first (html/select page [:body]))))
 
-(defn h3-sections [page]
-  (html/select page [:h3]))
+(defn extract-h3-sections [page]
+  (html/select page #{[:h3] [:p]}))
 
-(def first-section (first (h3-sections page)))
+(def h3-sections
+  "Remove the first P as it comes before the list of sections"
+  (rest  (extract-h3-sections page)))
+
+(defn is-h3? [input]  
+  (= :h3 (:tag  input)))
+
+(defn group-pairs [input]
+  "((:h3) (:p :p :p) (:h3 :p :p)) -> ((:h3 :p :p :p) (:h3 :p :p))"
+  (loop [in input out ()]
+    (if (empty? in)
+      out     
+      (recur (drop 2 in) (conj out (flatten (take 2 in)))))))
+
+(def sections  (reverse (group-pairs (partition-by is-h3? h3-sections))))
+
+(defn parse-status-text [status-text]
+  (let [matcher (re-matcher #" (\d+) (.*)" status-text)
+        groups (re-find matcher)]
+    [(second groups) (nth groups 2)]))
+
+(defn map-status-code [section]
+  (let [h3-tag (first section)
+        status-text (second (:content h3-tag))
+        [code text] (parse-status-text status-text)
+        section-id (:id (:attrs (first (:content (first section)))))] 
+    {:code code
+     :text text
+     :section section-id}))
+
+
--- test/status_codes/core_test.clj
+++ test/status_codes/core_test.clj
@@ -5,3 +5,19 @@
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
+(def test-data [:h3 :p :p :p :h3 :p :p :h3 :p])
+
+(def test-partitioned-data (partition-by header-test test-data))
+
+(print (map debug-el sections))
+
+(defn debug-el [el]
+  (str (:tag el) " " (html/text el) "\n"))
+
+(def first-section (first (h3-sections page)))
+
+(map :tag (nth sections 11))
+
+
+(def matcher (re-matcher #" (\d+) (.*)" " 505 HTTP Version Not Supported"))
